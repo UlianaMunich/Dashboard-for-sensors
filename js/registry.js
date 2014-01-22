5b380b73420c1971b33e5f7072a0daaf0f052ee1
Uliana <like-me1@yandex.ru>
2014-01-22 22:46:13 +0200

    test preview tag2

--- js/registry.js
+++ js/registry.js
@@ -59,5 +59,5 @@ function registry_parsing(sensor_json) {
         }
     });
     //update sensor list by clicking on Sensor List in nav.bar
-    $('#update_list').click(sensor_parsing);
+    $('#update_list').click(function(sensor_parsing));
 }
