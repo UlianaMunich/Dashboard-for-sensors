e1dc51c87835e209145e5a2aacb5f3932d38d138
Uliana <like-me1@yandex.ru>
2014-01-23 11:47:05 +0200

    Open Source dashboard

--- js/registry.js
+++ js/registry.js
@@ -33,7 +33,7 @@ function registry_parsing(sensor_json) {
     for (var i = 0; i < json.sensor_list.length; i++) {
         if (json.sensor_list[i].availability == true) {
             var sensor = json.sensor_list[i];
-                        $('#sensor_list').empty();
+            
 
             var tag_icon = "<span class='icon col-sm-2'><img width='20px' src='" + sensor.icon + "'></img></span>";
             var tag_title = "<span class='title col-sm-6'>" + sensor.title + "</span>";
@@ -54,6 +54,7 @@ function registry_parsing(sensor_json) {
                 var tag_sensor = "<div class='sensor col-md-4' id='" + sensor.id + "'><div class='row'>" + tag_icon + tag_title + tag_subscribe + "</div><div class='row'>" + tag_description + "</div></div>";
 
             }
+            $('#sensor_list').empty();
             $('#sensor_list').append(tag_sensor);
         }
     };
