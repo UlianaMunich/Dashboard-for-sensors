5ed9a883823a444d0f127dd0e4ab8e6359d12786
Uliana <like-me1@yandex.ru>
2014-01-23 15:52:17 +0200

    test of buttons3

--- js/registry.js
+++ js/registry.js
@@ -38,7 +38,7 @@ function registry_parsing(sensor_json) {
             var tag_icon = "<span class='icon col-sm-2'><img width='20px' src='" + sensor.icon + "'></img></span>";
             var tag_title = "<span class='title'><h3>" + sensor.title + "</h3></span>";
             var tag_description = "<span class='description'>" + sensor.description + "</span>";
-            var tag_subscribe = "<span class='subscribe col-sm-4'><button id='subscribe' class='btn btn-primary' type='button'>Subscribe</button><div class='sla'>" + sensor.sla + "</div></span>";
+            var tag_subscribe = "<span class='subscribe col-sm-4'><button id='subscribe' class='btn btn-primary' type='button' style='margin-top:10px'>Subscribe</button><div class='sla'>" + sensor.sla + "</div></span>";
             
 
             //check availability of preview if yes then sho preview if not substitude to tag_preview to tag_icon
